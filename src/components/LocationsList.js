e8b482793620cfcfe0707af4e926c85beadd805c
seongck <seongk@pm.me>
2019-08-16 20:10:51 -0700

    Add axios call and import LocationCard to LocationsList

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,27 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import LocationCard from './LocationCard.js';
 
-export default function LocationsList() {}
+export default function LocationsList(props) {
+  const [ locationList, setLocationList ] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/location/')
+      .then( res => {
+        console.log(res);
+        setLocationList(res.data.results);
+      })
+      .catch( err => {
+        console.log(err);
+      });
+  }, [props.match.params.path]);
+
+  return (
+    <section className="location-list grid-view">
+      { locationList.forEach( location => {
+        return <LocationCard key={location.id} {...location} />
+      })}
+    </section>
+  );
+
+}
