1b49c827df9e456be5a27960eeac30f6dd89156c
seongck <seongk@pm.me>
2019-08-16 20:05:57 -0700

    Add LocationCard elements

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Card, Button } from "semantic-ui-react";
 
 export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Description>{type} - {dimension}</Card.Description>
+        <Card.Content extra>
+          <div className="ui one buttons">
+            <Button>
+              {residents.length} residents
+            </Button>
+          </div>
+        </Card.Content>
+      </Card.Content>
+    </Card>
+  );
 }
+
+
