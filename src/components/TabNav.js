c042f060f56a15d7a227de0481a96839d8fa57f5
seongck <seongk@pm.me>
2019-08-16 19:13:33 -0700

    Add TabNav component

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -3,6 +3,24 @@ import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
-export default function TabNav() {
 
+const panes = [
+  { menuItem: {key: 'home', icon: 'home', content: 'Home Page' },
+    render: () => <Tab.Pane>Home Content</Tab.Pane>,
+  },
+  { menuItem: {key: 'characters', icon: 'users', content: 'Characters' },
+    render: () => <Tab.Pane>Characters Content</Tab.Pane>,
+  },
+  { menuItem: {key: 'locations', icon: 'map outline', content: 'Locations' },
+    render: () => <Tab.Pane>Locations Content</Tab.Pane>,
+  },
+  { menuItem: {key: 'episodes', icon: 'video camera', content: 'Episodes' },
+    render: () => <Tab.Pane>Episodes Content</Tab.Pane>,
+  },
+];
+
+export default function TabNav() {
+  return (
+    <Tab panes={panes} />
+  )
 };
