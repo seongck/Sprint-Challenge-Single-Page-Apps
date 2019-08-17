8360d5db4f7e4aec746dc2919dbc571137e08a09
seongck <seongk@pm.me>
2019-08-16 18:58:14 -0700

    Add temporary comments to fix rendering errors

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <main>
       <Header />
-      <TabNav />
-      <AppRouter />
+    {/*  <TabNav /> */}
+    {/*<AppRouter /> */}
     </main>
   );
 }
