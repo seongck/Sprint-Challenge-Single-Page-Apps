22748554cfcdaf1aa73eed3e571b35d152bdbd3d
seongck <seongk@pm.me>
2019-08-16 21:04:19 -0700

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-//import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
   return (
