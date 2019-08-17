bef21d024e7d5d491eaaa370a4b1427e1247f2ba
seongck <seongk@pm.me>
2019-08-16 19:41:59 -0700

    Add axios fetch request

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
+  const [ characterList, setCharacterList ] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then( res => {
+        console.log(res);
+        setCharacterList(res.data.results);
+      })
+      .catch( err => {
+        console.log(err);
+      });
+  }, [{/*TODO */}]);
 
   return (
     <section className="character-list grid-view">
